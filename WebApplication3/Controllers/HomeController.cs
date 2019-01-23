fedb539595030c81a29e099719147b1316dd6a87
IKaskov <45712156+IKaskov@users.noreply.github.com>
2019-01-23 12:25:26 +0200

    Initial commit

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        //BookContext db = new BookContext();
+
+        IBookRepository repository = new BookRepository();
+
+        public HomeController()
+        {
+        }
+
+        public HomeController(IBookRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public ActionResult Data()
+        {
+            ViewBag.Title = "Books Data";
+
+            return View(repository.GetAll());
+        }
+        
+    }
+}
---
+++ WebApplication3/Models/BookDBInitializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class BookDbInitializer : CreateDatabaseIfNotExists<BookContext>
+    //CreateDatabaseIfNotExists<BookContext>
+    //DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Война и мир", Author = "Л. Толстой", Year = 1863 });
+            db.Books.Add(new Book { Name = "Отцы и дети", Author = "И. Тургенев", Year = 1862 });
+            db.Books.Add(new Book { Name = "Чайка", Author = "А. Чехов", Year = 1896 });
+
+            base.Seed(db);
+        }
+    }
+}
